e4bf55825261ddd70325533e4e8499322593add1
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2020-11-29 16:45:03 +0000

    Create stale.yml

--- app/src/androidTest/kotlin/com/evastos/music/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/com/evastos/music/ExampleInstrumentedTest.kt
@@ -2,4 +2,9 @@ package com.evastos.music
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-imp
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an A
