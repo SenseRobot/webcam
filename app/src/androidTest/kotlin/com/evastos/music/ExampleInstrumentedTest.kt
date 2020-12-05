5d687f9529f5102e941fd93a24f98dc219bad83a
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2020-12-05 22:02:35 +0000

    f2fs: split drop_inmem_pages from commit_inmem_pages

--- app/src/androidTest/kotlin/com/evastos/music/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/com/evastos/music/ExampleInstrumentedTest.kt
@@ -7,4 +7,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 /**
- * Instrumented test, which will execute on an A
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumen
