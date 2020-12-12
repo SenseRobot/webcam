e48209edc6a873e1811e12613dcdd6f7154ce8fb
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2020-12-12 12:00:46 +0000

    Fix selection

--- app/src/androidTest/kotlin/com/evastos/music/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/com/evastos/music/ExampleInstrumentedTest.kt
@@ -12,4 +12,8 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumen
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = Instrumentation
