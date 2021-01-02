6f8820edca1c54ba2bb13086f645d220f168dd3f
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-01-02 07:26:00 +0000

    upgrade version.

--- app/src/main/kotlin/com/evastos/music/data/model/authentication/AuthData.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/authentication/AuthData.kt
@@ -1,3 +1,8 @@
 package com.evastos.music.data.model.authentication
 
-import com.squareup.moshi.Js
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class AuthData(
+    @Json(name = "authToken") val authToken: S
