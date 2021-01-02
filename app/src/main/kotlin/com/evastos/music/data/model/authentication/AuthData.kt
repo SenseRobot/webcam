7dcfb060aec6d0cba2098041bbc2ac464b17dcae
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-01-02 12:51:24 +0000

    add metrics

--- app/src/main/kotlin/com/evastos/music/data/model/authentication/AuthData.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/authentication/AuthData.kt
@@ -5,4 +5,6 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class AuthData(
-    @Json(name = "authToken") val authToken: S
+    @Json(name = "authToken") val authToken: String,
+    @Json(name = "authTokenExpiresIn") val authTokenExpiresIn: Long, // millis
+    @Json(name = "authTokenRefreshedAt") val auth
