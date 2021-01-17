e1cdbd56315bab913417ce7d869efa34d28c27af
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-01-17 20:36:04 +0000

    unpublish

--- app/src/main/kotlin/com/evastos/music/data/model/spotify/item/Items.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/spotify/item/Items.kt
@@ -5,4 +5,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Items<T>(
-    @Json(name =
+    @Json(name = "href") val href: String?,
+    @Json(name = "items") val items: List<T>?,
+    @Json(name = "limit") val limit: Int?,
+  
