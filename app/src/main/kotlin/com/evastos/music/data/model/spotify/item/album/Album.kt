b9cbc455cd646ad619b4d21410c8ec2d950c523f
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-01-23 11:12:03 +0000

    link change

--- app/src/main/kotlin/com/evastos/music/data/model/spotify/item/album/Album.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/spotify/item/album/Album.kt
@@ -1,1 +1,9 @@
-package com.evastos.music.data.model.spotify
+package com.evastos.music.data.model.spotify.item.album
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * Album Spotify item.
+ */
+@JsonClass(gener
