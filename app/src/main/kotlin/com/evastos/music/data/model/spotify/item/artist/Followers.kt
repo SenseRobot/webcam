99538d1d73cc91628bbbfbe350e9fa18047a4b60
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-01-30 19:53:23 +0000

    change log level

--- app/src/main/kotlin/com/evastos/music/data/model/spotify/item/artist/Followers.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/spotify/item/artist/Followers.kt
@@ -3,4 +3,11 @@ package com.evastos.music.data.model.spotify.item.artist
 import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotl
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * Followers Spotify item.
+ */
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class Fol
