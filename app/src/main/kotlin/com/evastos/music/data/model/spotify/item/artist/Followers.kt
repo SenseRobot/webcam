74c0494d36bfae587ee2ead141834944ea5cf599
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-01-30 16:43:09 +0000

    added build badge

--- app/src/main/kotlin/com/evastos/music/data/model/spotify/item/artist/Followers.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/spotify/item/artist/Followers.kt
@@ -1,1 +1,6 @@
-package com.evastos.music
+package com.evastos.music.data.model.spotify.item.artist
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotl
