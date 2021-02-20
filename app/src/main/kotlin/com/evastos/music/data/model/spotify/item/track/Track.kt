e0221bbc026a503e0513c5a9407862bfa3de5e1b
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-02-20 23:05:59 +0000

    Change readme

--- app/src/main/kotlin/com/evastos/music/data/model/spotify/item/track/Track.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/spotify/item/track/Track.kt
@@ -1,1 +1,9 @@
-package com.evastos.music.data.
+package com.evastos.music.data.model.spotify.item.track
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * Track Spotify item.
+ */
+@Json
