a4342e12d71efa9a51d6762358ba59ba16af6073
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-02-27 05:42:19 +0000

    chore(package): update sinon to version 1.17.5

--- app/src/main/kotlin/com/evastos/music/data/model/spotify/search/ExternalUrls.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/spotify/search/ExternalUrls.kt
@@ -1,1 +1,6 @@
-package com.evastos.music.data.model.spo
+package com.evastos.music.data.model.spotify.search
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.android.
