658454753645233cbee3c06d077f2d7863bfa430
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-02-27 07:20:39 +0000

    bump version.

--- app/src/main/kotlin/com/evastos/music/data/model/spotify/search/ExternalUrls.kt
+++ app/src/main/kotlin/com/evastos/music/data/model/spotify/search/ExternalUrls.kt
@@ -3,4 +3,12 @@ package com.evastos.music.data.model.spotify.search
 import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlinx.android.
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * External URLs.
+ */
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class ExternalUrls(
+    @Json(name = "spotify") val s
