860f36bf3b23718584162d754f87679ab76e1ef7
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-03-07 08:11:48 +0000

    bump to v0.3.0

--- app/src/main/kotlin/com/evastos/music/data/network/connectivity/NetworkConnectivityProvider.kt
+++ app/src/main/kotlin/com/evastos/music/data/network/connectivity/NetworkConnectivityProvider.kt
@@ -1,4 +1,8 @@
 package com.evastos.music.data.network.connectivity
 
 import android.content.Context
-imp
+import android.net.ConnectivityManager
+import com.evastos.music.inject.qualifier.AppContext
+import javax.inject.Inject
+
+open class NetworkConnectivi
