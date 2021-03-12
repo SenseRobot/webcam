a1f4d3f21374b77e50a1779cecee573452ab2d79
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-03-12 12:25:32 +0000

    Minor code clean up

--- app/src/main/kotlin/com/evastos/music/data/network/connectivity/NetworkConnectivityProvider.kt
+++ app/src/main/kotlin/com/evastos/music/data/network/connectivity/NetworkConnectivityProvider.kt
@@ -5,4 +5,7 @@ import android.net.ConnectivityManager
 import com.evastos.music.inject.qualifier.AppContext
 import javax.inject.Inject
 
-open class NetworkConnectivi
+open class NetworkConnectivityProvider
+@Inject constructor(@AppContext private val context: Context) {
+
+    open fun isConnected(): B
