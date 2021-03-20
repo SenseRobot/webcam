beb8cbfbd5c13a187bb760980b3684802781362f
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-03-20 17:20:00 +0000

    netfilter: xt_qtaguid: fix memory leak in seq_file handlers

--- app/src/main/kotlin/com/evastos/music/data/network/interceptor/HeadersInterceptor.kt
+++ app/src/main/kotlin/com/evastos/music/data/network/interceptor/HeadersInterceptor.kt
@@ -2,4 +2,9 @@ package com.evastos.music.data.network.interceptor
 
 import okhttp3.Interceptor
 import okhttp3.Response
-import javax.inje
+import javax.inject.Inject
+
+class HeadersInterceptor @Inject constructor() : Interceptor {
+
+    companion object {
+        private c
