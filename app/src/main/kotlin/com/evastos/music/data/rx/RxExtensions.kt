3fc3c9b66fb2b03a7ef46e6ed30b7b50e0179952
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-03-21 14:20:47 +0000

    Update flags

--- app/src/main/kotlin/com/evastos/music/data/rx/RxExtensions.kt
+++ app/src/main/kotlin/com/evastos/music/data/rx/RxExtensions.kt
@@ -6,4 +6,8 @@ import com.evastos.music.data.network.connectivity.NetworkConnectivityProvider
 import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.Single
-import io.rea
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+private cons
