d688e97abce071738ba5242dde765652b70be686
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-03-21 06:28:01 +0000

    Removed unused module

--- app/src/main/kotlin/com/evastos/music/data/rx/RxExtensions.kt
+++ app/src/main/kotlin/com/evastos/music/data/rx/RxExtensions.kt
@@ -1,3 +1,5 @@
 package com.evastos.music.data.rx
 
-import com.evastos.music.data.exception.Ex
+import com.evastos.music.data.exception.ExceptionMapper
+import com.evastos.music.data.exception.network.NetworkFailFastException
+import com.evastos.music.data.network.connect
