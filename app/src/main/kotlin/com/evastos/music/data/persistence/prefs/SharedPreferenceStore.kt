0ab4aa09238965464c69d398a09c406ad9dfb160
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-03-21 05:35:58 +0000

    New translations translation.json (Portuguese)

--- app/src/main/kotlin/com/evastos/music/data/persistence/prefs/SharedPreferenceStore.kt
+++ app/src/main/kotlin/com/evastos/music/data/persistence/prefs/SharedPreferenceStore.kt
@@ -26,3 +26,72 @@ class SharedPreferenceStore(
         set(value) {
             sharedPreferences[PreferenceStore.Constants.AUTH_DATA] = value
         }
+
+    override var user: User?
+        get() = sharedPreferences[PreferenceStore.Constants.USER]
+        set(value) {
+            sharedPreferences[PreferenceStore.Constants.USER] = value
+        }
+
+    override var artistQuery: String?
+        get() = sharedPreferences[PreferenceStore.Constants.ARTIST_QUERY]
+        set(value) {
+            sharedPreferences[PreferenceStore.Constants.ARTIST_QUERY] = value
+        }
+
+    operator fun SharedPreferences.set(key: String, value: Any?) {
+        when (value) {
+            is String? -> edit { it.putString(key, value) }
+            is Int -> edit { it.putInt(key, value) }
+            is Boolean -> edit { it.putBoolean(key, value) }
+            is Float -> edit { it.putFloat(key, value) }
+            is Long -> edit { it.putLong(key, value) }
+            is AuthData -> {
+                val authDataJson = moshi.adapter(AuthData::class.java).toJson(value)
+                edit {
+                    it.putString(key, authDataJson)
+                }
+            }
+            is User -> {
+                val userJson = moshi.adapter(User::class.java).toJson(value)
+                edit {
+                    it.putString(key, userJson)
+                }
+            }
+            else -> throw UnsupportedOperationException("Not yet implemented")
+        }
+    }
+
+    @Suppress("UnsafeCast")
+    inline operator fun <reified T : Any> SharedPreferences.get(
+        key: String,
+        defaultValue: T? = null
+    ): T? {
+        return when (T::class) {
+            String::class -> getString(key, defaultValue as? String) as T?
+            Int::class -> getInt(key, defaultValue as? Int ?: -1) as T?
+            Boolean::class -> getBoolean(key, defaultValue as? Boolean ?: false) as T?
+            Float::class -> getFloat(key, defaultValue as? Float ?: -1f) as T?
+            Long::class -> getLong(key, defaultValue as? Long ?: -1) as T?
+            AuthData::class -> getFromJson(key, defaultValue)
+            User::class -> getFromJson(key, defaultValue)
+            else -> throw UnsupportedOperationException("Not yet implemented")
+        }
+    }
+
+    private inline fun SharedPreferences.edit(operation: (SharedPreferences.Editor) -> Unit) {
+        val editor = this.edit()
+        operation(editor)
+        editor.apply()
+    }
+
+    inline fun <reified T : Any> SharedPreferences.getFromJson(
+        key: String,
+        defaultValue: T? = null
+    ): T? {
+        getString(key, defaultValue as? String)?.let {
+            return Moshi.Builder().build().adapter(T::class.java).fromJson(it)
+        }
+        return null
+    }
+}
---
+++ app/src/main/kotlin/com/evastos/music/data/rx/RxExtensions.kt
@@ -0,0 +1,3 @@
+package com.evastos.music.data.rx
+
+import com.evastos.music.data.exception.Ex
