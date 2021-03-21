91a84a45e3333764c82200f1aac75ca801f950b8
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-03-21 11:06:16 +0000

    clean up project

--- app/src/main/kotlin/com/evastos/music/data/rx/RxExtensions.kt
+++ app/src/main/kotlin/com/evastos/music/data/rx/RxExtensions.kt
@@ -2,4 +2,8 @@ package com.evastos.music.data.rx
 
 import com.evastos.music.data.exception.ExceptionMapper
 import com.evastos.music.data.exception.network.NetworkFailFastException
-import com.evastos.music.data.network.connect
+import com.evastos.music.data.network.connectivity.NetworkConnectivityProvider
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.rea
