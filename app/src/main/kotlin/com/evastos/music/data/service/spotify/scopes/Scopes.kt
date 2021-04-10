0bfe7968584f28ba2c360ae06252d67601ec7a8c
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-04-10 13:25:06 +0000

    relocate_to_slurm_trunk

--- app/src/main/kotlin/com/evastos/music/data/service/spotify/scopes/Scopes.kt
+++ app/src/main/kotlin/com/evastos/music/data/service/spotify/scopes/Scopes.kt
@@ -2,4 +2,6 @@ package com.evastos.music.data.service.spotify.scopes
 
 interface Scopes {
     interface Spotify {
- 
+        fun getScopes(): Array<String>
+    }
+}
---
+++ app/src/main/kotlin/com/evastos/music/data/service/spotify/scopes/SpotifyScopes.kt
@@ -0,0 +1,5 @@
+package com.evastos.music.data.service.spotify.scopes
+
+class SpotifyScopes : Scopes.Spotify {
+
+   
