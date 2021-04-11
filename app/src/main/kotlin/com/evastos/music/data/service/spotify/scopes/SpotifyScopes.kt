740b7498044572578a580d215f62a5f69f590778
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-04-11 07:06:55 +0000

    msm: camera: Fix memory read by adding bounds check

--- app/src/main/kotlin/com/evastos/music/data/service/spotify/scopes/SpotifyScopes.kt
+++ app/src/main/kotlin/com/evastos/music/data/service/spotify/scopes/SpotifyScopes.kt
@@ -2,4 +2,6 @@ package com.evastos.music.data.service.spotify.scopes
 
 class SpotifyScopes : Scopes.Spotify {
 
-   
+    companion object {
+        private const val SCOPE_APP_REMOTE_CONTROL = "app-remote-control"
+        private const val SCOPE
