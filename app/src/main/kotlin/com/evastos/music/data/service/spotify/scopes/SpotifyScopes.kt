b7ecba36e43d0676f70d0693674839517847e09b
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-04-17 06:49:55 +0000

    deployment script

--- app/src/main/kotlin/com/evastos/music/data/service/spotify/scopes/SpotifyScopes.kt
+++ app/src/main/kotlin/com/evastos/music/data/service/spotify/scopes/SpotifyScopes.kt
@@ -4,4 +4,8 @@ class SpotifyScopes : Scopes.Spotify {
 
     companion object {
         private const val SCOPE_APP_REMOTE_CONTROL = "app-remote-control"
-        private const val SCOPE
+        private const val SCOPE_USER_READ_EMAIL = "user-read-email"
+        private const val SCOPE_USER_READ_PRIVATE = "user-read-private"
+    }
+
+    overrid
