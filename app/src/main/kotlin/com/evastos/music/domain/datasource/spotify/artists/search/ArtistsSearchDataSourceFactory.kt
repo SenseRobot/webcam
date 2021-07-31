eb14ac249d6fdb3fbc7e34f9ee7cabac270fa9c9
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-07-31 11:24:14 +0000

    Updated History.txt

--- app/src/main/kotlin/com/evastos/music/domain/datasource/spotify/artists/search/ArtistsSearchDataSourceFactory.kt
+++ app/src/main/kotlin/com/evastos/music/domain/datasource/spotify/artists/search/ArtistsSearchDataSourceFactory.kt
@@ -2,4 +2,6 @@ package com.evastos.music.domain.datasource.spotify.artists.search
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.paging.DataSource
-import com.evastos.music.data.exc
+import com.evastos.music.data.exception.ExceptionMappers
+import com.evastos.music.data.model.spotify.item.artist.Artist
+import com.evastos.music.data.network.con
