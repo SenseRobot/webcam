76682963f8f1dee0a2c2384decd34196faa5c0b1
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-08-01 18:21:34 +0000

    ASoC: sgtl5000: add delay before first I2C access

--- app/src/main/kotlin/com/evastos/music/domain/datasource/spotify/artists/search/ArtistsSearchDataSourceFactory.kt
+++ app/src/main/kotlin/com/evastos/music/domain/datasource/spotify/artists/search/ArtistsSearchDataSourceFactory.kt
@@ -4,4 +4,6 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.paging.DataSource
 import com.evastos.music.data.exception.ExceptionMappers
 import com.evastos.music.data.model.spotify.item.artist.Artist
-import com.evastos.music.data.network.con
+import com.evastos.music.data.network.connectivity.NetworkConnectivityProvider
+import com.evastos.music.data.service.spotify.SpotifyService
+import com.evastos.music.domain
