f9fda144febe122eb6b52951207b68b98dfb9d26
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-10-02 17:13:21 +0000

    Added simple readme

--- app/src/main/kotlin/com/evastos/music/domain/exception/spotify/SpotifyExceptionMessageProvider.kt
+++ app/src/main/kotlin/com/evastos/music/domain/exception/spotify/SpotifyExceptionMessageProvider.kt
@@ -2,4 +2,6 @@ package com.evastos.music.domain.exception.spotify
 
 import android.content.Context
 import com.evastos.music.R
-import com.evastos.music.data.exception.spotify.SpotifyExcept
+import com.evastos.music.data.exception.spotify.SpotifyException
+import com.evastos.music.domain.exception.ExceptionMessageProviders
+import com.evastos.music.inject.qualifier.App
