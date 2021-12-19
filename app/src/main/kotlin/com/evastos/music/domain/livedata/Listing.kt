d82125fd194678ba230ddd4e9ca007105502d289
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-12-19 05:59:25 +0000

    Removing debug output

--- app/src/main/kotlin/com/evastos/music/domain/livedata/Listing.kt
+++ app/src/main/kotlin/com/evastos/music/domain/livedata/Listing.kt
@@ -20,4 +20,8 @@ import android.arch.lifecycle.LiveData
 import android.arch.paging.PagedList
 
 /**
- * Data class that is necessary for a UI to show a listing and interact w/ the rest of the
+ * Data class that is necessary for a UI to show a listing and interact w/ the rest of the system
+ */
+data class Listing<T>(
+    // the LiveData of paged lists for the UI to observe
+    val pagedList: LiveData<PagedList<T
