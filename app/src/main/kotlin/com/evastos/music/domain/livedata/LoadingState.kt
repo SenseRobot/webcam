3779ce24b6cba2319e84feb1bdd65ab693a7e7b1
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2021-12-26 18:46:52 +0000

    Some more styling

--- app/src/main/kotlin/com/evastos/music/domain/livedata/LoadingState.kt
+++ app/src/main/kotlin/com/evastos/music/domain/livedata/LoadingState.kt
@@ -3,4 +3,7 @@ package com.evastos.music.domain.livedata
 /**
  * Indicates the loading state of the UI.
  */
-sealed class Loading
+sealed class LoadingState {
+    class Loading : LoadingState()
+    class Success : LoadingState()
+    class Error(val errorMessage: S
