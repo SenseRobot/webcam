8adf6ef9964acfe36fc48c8afcea30863c5890c9
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-01-12 18:22:43 +0000

    sata_promise: fix hardreset lockdep error

--- app/src/main/kotlin/com/evastos/music/domain/livedata/SingleLiveEvent.kt
+++ app/src/main/kotlin/com/evastos/music/domain/livedata/SingleLiveEvent.kt
@@ -11,4 +11,6 @@ package com.evastos.music.domain.livedata
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITION
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under th
