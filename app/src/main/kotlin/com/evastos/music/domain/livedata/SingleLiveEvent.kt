4fc631038d0a91f9f28ed83fd2a584e1dfa25295
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-01-17 12:37:56 +0000

    remove test.txt

--- app/src/main/kotlin/com/evastos/music/domain/livedata/SingleLiveEvent.kt
+++ app/src/main/kotlin/com/evastos/music/domain/livedata/SingleLiveEvent.kt
@@ -25,4 +25,8 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * A lifecycle-aware observable that sends only new updates after subscription, used for events like
- * navigation and
+ * navigation and Snackbar messages.
+ *
+ *
+ * This avoids a common problem with events: on configuration change (like rotation) an update
+ * can be emitted if t
