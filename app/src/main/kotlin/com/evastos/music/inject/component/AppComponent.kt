8ffe4f41b29434e392b63b7f0f7e16180e691525
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-02-13 22:10:12 +0000

    added travis ci

--- app/src/main/kotlin/com/evastos/music/inject/component/AppComponent.kt
+++ app/src/main/kotlin/com/evastos/music/inject/component/AppComponent.kt
@@ -3,4 +3,6 @@ package com.evastos.music.inject.component
 import com.evastos.music.MusicApp
 import com.evastos.music.inject.module.ActivityBuilder
 import com.evastos.music.inject.module.AppModule
-impor
+import com.evastos.music.inject.module.ExceptionModule
+import com.evastos.music.inject.module.NetworkModule
+import com.evastos.music.inject.module.P
