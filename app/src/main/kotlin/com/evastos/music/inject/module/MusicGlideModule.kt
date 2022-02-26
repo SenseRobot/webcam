b3ea0052f3b670f0bc70f72df3b44d261c4d0e45
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-02-26 12:35:39 +0000

    tomcat

--- app/src/main/kotlin/com/evastos/music/inject/module/MusicGlideModule.kt
+++ app/src/main/kotlin/com/evastos/music/inject/module/MusicGlideModule.kt
@@ -1,3 +1,8 @@
 package com.evastos.music.inject.module
 
-import com.bumptech.glide.annotation.Gli
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+// new since Glide v4
+@GlideModule
+class MusicGlideModu
