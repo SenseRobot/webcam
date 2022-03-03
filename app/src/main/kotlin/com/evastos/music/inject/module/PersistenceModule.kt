c2b493a8ad8800ab6f3c05d98401e53a01a56ad9
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-03-03 17:14:59 +0000

    Updated package.json file

--- app/src/main/kotlin/com/evastos/music/inject/module/PersistenceModule.kt
+++ app/src/main/kotlin/com/evastos/music/inject/module/PersistenceModule.kt
@@ -2,4 +2,8 @@ package com.evastos.music.inject.module
 
 import android.content.Context
 import com.evastos.music.data.persistence.prefs.PreferenceStore
-import com.evastos.music.data.persistence.prefs
+import com.evastos.music.data.persistence.prefs.SharedPreferenceStore
+import com.evastos.music.inject.qualifier.AppContext
+import dagger.Module
+import dagger.Provides
+import javax.inject
