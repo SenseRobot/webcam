345d7109af47c322af7e87d176c49a2d417f7150
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-03-04 21:39:56 +0000

    Updated version in readme

--- app/src/main/kotlin/com/evastos/music/inject/module/ReceiverBuilder.kt
+++ app/src/main/kotlin/com/evastos/music/inject/module/ReceiverBuilder.kt
@@ -1,1 +1,5 @@
-package com.evastos.music.inject.m
+package com.evastos.music.inject.module
+
+import com.evastos.music.data.network.connectivity.NetworkConnectivityReceiver
+import dagger.Module
+import dagger.and
