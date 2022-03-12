5fb43fbdf8f5337ad965fb42f5563ef59d6be725
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-03-12 15:38:37 +0000

    lr

--- app/src/main/kotlin/com/evastos/music/inject/module/ViewModelModule.kt
+++ app/src/main/kotlin/com/evastos/music/inject/module/ViewModelModule.kt
@@ -1,1 +1,6 @@
-package com.evastos.music.inject.modu
+package com.evastos.music.inject.module
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import com.evastos.music.inject.qualifier.ViewModelKey
+import com.eva
