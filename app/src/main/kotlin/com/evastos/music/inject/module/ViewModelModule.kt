d1bd5d3a392bf8fefbfcfd3e803d8a9712a4ea04
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-03-16 08:08:06 +0000

    Latest OOV and WER numbers

--- app/src/main/kotlin/com/evastos/music/inject/module/ViewModelModule.kt
+++ app/src/main/kotlin/com/evastos/music/inject/module/ViewModelModule.kt
@@ -3,4 +3,6 @@ package com.evastos.music.inject.module
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import com.evastos.music.inject.qualifier.ViewModelKey
-import com.eva
+import com.evastos.music.inject.viewmodel.ViewModelFactory
+import com.evastos.music.ui.spotify.artists.ArtistsViewModel
+import com.evastos.music.ui.spotify.artists.
