7815a1988d183575b05a81fe52fb06ee81fc6421
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-04-01 13:19:27 +0000

    remove pdb files

--- app/src/main/kotlin/com/evastos/music/inject/qualifier/ViewModelKey.kt
+++ app/src/main/kotlin/com/evastos/music/inject/qualifier/ViewModelKey.kt
@@ -7,4 +7,9 @@ import kotlin.reflect.KClass
 @MustBeDocumented
 @Target(
     AnnotationTarget.FUNCTION,
-    A
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+ann
