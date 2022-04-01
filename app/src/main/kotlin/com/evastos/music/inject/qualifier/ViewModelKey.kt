634d81b6105d822b052e77247b38fc1fbacab2f6
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-04-01 09:21:14 +0000

    Update 1-col-portfolio.css

--- app/src/main/kotlin/com/evastos/music/inject/qualifier/ViewModelKey.kt
+++ app/src/main/kotlin/com/evastos/music/inject/qualifier/ViewModelKey.kt
@@ -1,3 +1,10 @@
 package com.evastos.music.inject.qualifier
 
-import android.arch.life
+import android.arch.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@MustBeDocumented
+@Target(
+    AnnotationTarget.FUNCTION,
+    A
