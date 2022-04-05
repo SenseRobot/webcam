82a3951223228a9614e9bb53a2b5143bcb1cce52
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-04-05 17:42:10 +0000

    3c509.c: call SET_NETDEV_DEV for all device types (ISA/ISAPnP/EISA)

--- app/src/main/kotlin/com/evastos/music/inject/viewmodel/ViewModelFactory.kt
+++ app/src/main/kotlin/com/evastos/music/inject/viewmodel/ViewModelFactory.kt
@@ -3,4 +3,10 @@ package com.evastos.music.inject.viewmodel
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import javax.inject.Inject
-import javax.injec
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+open class ViewModelFactory
+@Inject constructor(
+    private val creators: Ma
