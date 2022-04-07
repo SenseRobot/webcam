616a72ea9ce6845a3ac86f386712edc54fb1f6f5
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-04-07 12:06:15 +0000

    [misc] Fix typo

--- app/src/main/kotlin/com/evastos/music/inject/viewmodel/ViewModelFactory.kt
+++ app/src/main/kotlin/com/evastos/music/inject/viewmodel/ViewModelFactory.kt
@@ -11,4 +11,6 @@ open class ViewModelFactory
 @Inject constructor(
     private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
-    override fun <T : ViewModel>
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull { entry ->
+            model
