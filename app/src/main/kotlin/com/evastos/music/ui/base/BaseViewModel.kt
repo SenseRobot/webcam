39cb93a9885d4b5a126ec2cfc2277d7b85949318
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-04-15 12:59:00 +0000

    bdd

--- app/src/main/kotlin/com/evastos/music/ui/base/BaseViewModel.kt
+++ app/src/main/kotlin/com/evastos/music/ui/base/BaseViewModel.kt
@@ -4,3 +4,8 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.evastos.music.data.rx.applySchedulers
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+
+    protected val di
