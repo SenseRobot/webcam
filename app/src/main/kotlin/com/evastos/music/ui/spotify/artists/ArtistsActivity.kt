0b5afbaa482ee968498b2ebc876613d787993147
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-04-18 21:12:23 +0000

    MIT Licensed

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/ArtistsActivity.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/ArtistsActivity.kt
@@ -11,3 +11,125 @@ import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.SearchView
 import android.view.Menu
+import com.evastos.music.R
+import com.evastos.music.data.model.spotify.item.artist.Artist
+import com.evastos.music.domain.livedata.LoadingState
+import com.evastos.music.inject.module.GlideApp
+import com.evastos.music.ui.base.BaseActivity
+import com.evastos.music.ui.spotify.artists.adapter.ArtistsAdapter
+import com.evastos.music.ui.spotify.artists.adapter.suggestions.ArtistSuggestionsAdapter
+import com.evastos.music.ui.spotify.artists.details.ArtistDetailsActivity
+import com.evastos.music.ui.util.extensions.setGone
+import com.evastos.music.ui.util.extensions.setNotRefreshing
+import com.evastos.music.ui.util.extensions.setRefreshing
+import com.evastos.music.ui.util.extensions.setVisible
+import kotlinx.android.synthetic.main.activity_artists.artistsLoadingView
+import kotlinx.android.synthetic.main.activity_artists.artistsRecyclerView
+import kotlinx.android.synthetic.main.activity_artists.artistsRootView
+import kotlinx.android.synthetic.main.activity_artists.artistsSwipeRefresh
+import kotlinx.android.synthetic.main.activity_artists.networkConnectivityBanner
+
+class ArtistsActivity : BaseActivity() {
+
+    companion object {
+        private const val ARTISTS_COLUMN_COUNT = 2
+
+        fun newIntent(context: Context) = Intent(context, ArtistsActivity::class.java)
+    }
+
+    override val layoutRes: Int = R.layout.activity_artists
+
+    private lateinit var viewModel: ArtistsViewModel
+
+    private lateinit var artistSuggestionsAdapter: ArtistSuggestionsAdapter
+
+    private lateinit var artistsAdapter: ArtistsAdapter
+
+    private var searchView: SearchView? = null
+
+    @SuppressLint("CheckResult")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.apply {
+            title = getString(R.string.activity_title_artists)
+            setDisplayHomeAsUpEnabled(false)
+        }
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(ArtistsViewModel::class.java)
+
+        artistsRecyclerView.apply {
+            artistsAdapter = ArtistsAdapter(GlideApp.with(this)) { artist: Artist? ->
+                viewModel.onArtistClick(artist)
+            }
+            layoutManager = GridLayoutManager(context, ARTISTS_COLUMN_COUNT)
+            adapter = artistsAdapter
+        }
+
+        artistSuggestionsAdapter = ArtistSuggestionsAdapter(this)
+
+        artistsSwipeRefresh.setOnRefreshListener {
+            artistsSwipeRefresh.setRefreshing()
+            viewModel.onRefresh()
+        }
+
+        viewModel.artistsLiveData.observe(this, Observer<PagedList<Artist>> { pagedArtists ->
+            artistsAdapter.submitList(pagedArtists)
+        })
+
+        viewModel.loadingStateLiveData.observe(this, Observer { loadingState ->
+            loadingState?.let { it ->
+                when (it) {
+                    is LoadingState.Loading -> {
+                        artistsLoadingView.setVisible()
+                        hideSnackbar()
+                    }
+                    is LoadingState.Success -> {
+                        artistsLoadingView.setGone()
+                        artistsSwipeRefresh.setNotRefreshing()
+                        hideSnackbar()
+                    }
+                    is LoadingState.Error -> {
+                        artistsLoadingView.setGone()
+                        artistsSwipeRefresh.setNotRefreshing()
+                        it.errorMessage?.let { error ->
+                            showSnackbar(artistsRootView, error, getString(R.string.action_retry)) {
+                                viewModel.onRetry()
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        viewModel.artistSuggestionsLiveData.observe(this, Observer { suggestions ->
+            artistSuggestionsAdapter.setSuggestions(suggestions)
+        })
+
+        viewModel.artistDetailsLiveData.observe(this, Observer { artist ->
+            artist?.let {
+                startActivity(ArtistDetailsActivity.newIntent(this, it))
+            }
+        })
+
+        viewModel.artistSearchLiveData.observe(this, Observer { artistName ->
+            supportActionBar?.apply {
+                this.subtitle = artistName
+            }
+        })
+
+        viewModel.networkConnectivityBannerLiveData.observe(this, Observer { isVisible ->
+            if (isVisible == true) {
+                networkConnectivityBanner.setVisible()
+            } else networkConnectivityBanner.setGone()
+        })
+
+        viewModel.onCreate(networkConnectivityReceiver.observable)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_activity_artists, menu)
+        val searchItem = menu.findItem(R.id.searchArtistsAction)
+
+        searchView = searchItem.actionView as? SearchView
+        searchView?.apply {
