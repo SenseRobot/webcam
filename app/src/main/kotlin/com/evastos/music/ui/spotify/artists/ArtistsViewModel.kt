6653c1456021b3488a723813f9f097d8fa426483
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-04-24 21:29:39 +0000

    clean up deps

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/ArtistsViewModel.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/ArtistsViewModel.kt
@@ -3,4 +3,7 @@ package com.evastos.music.ui.spotify.artists
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MediatorLiveData
 import android.arch.paging.PagedList
-import com.evastos.music.dat
+import com.evastos.music.data.model.spotify.item.artist.Artist
+import com.evastos.music.domain.Repositories
+import com.evastos.music.domain.livedata.Listing
+import com.eva
