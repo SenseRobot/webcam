f39df8d19726e0d51c90bb3e2535fcf4a368e2b0
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-06-02 10:43:58 +0000

    Added initial tests

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistDiffItemCallback.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistDiffItemCallback.kt
@@ -1,4 +1,7 @@
 package com.evastos.music.ui.spotify.artists.adapter
 
 import android.support.v7.util.DiffUtil
-import com.evastos.music.data.model.spotify.item.artist.Ar
+import com.evastos.music.data.model.spotify.item.artist.Artist
+
+object ArtistDiffItemCallback : DiffUtil.ItemCallback<Artist>() {
+    override fun areItemsTheSame(oldItem: Artist, newItem: Artist): Boo
