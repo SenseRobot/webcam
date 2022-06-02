e78b0e7f53ae295ee87ad29a07ede0af0155bcd4
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-06-02 18:52:04 +0000

    add buttons

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistDiffItemCallback.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistDiffItemCallback.kt
@@ -4,4 +4,9 @@ import android.support.v7.util.DiffUtil
 import com.evastos.music.data.model.spotify.item.artist.Artist
 
 object ArtistDiffItemCallback : DiffUtil.ItemCallback<Artist>() {
-    override fun areItemsTheSame(oldItem: Artist, newItem: Artist): Boo
+    override fun areItemsTheSame(oldItem: Artist, newItem: Artist): Boolean {
+        return oldItem.id === newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Artist, newItem: Artist): Boolean {
+      
