d394c95617c3d897f7dd5b161a6a9cd9a1b6032f
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-06-07 16:41:43 +0000

    release commit

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistsAdapter.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistsAdapter.kt
@@ -1,3 +1,7 @@
 package com.evastos.music.ui.spotify.artists.adapter
 
-import android.arch.paging.Pa
+import android.arch.paging.PagedListAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.evas
