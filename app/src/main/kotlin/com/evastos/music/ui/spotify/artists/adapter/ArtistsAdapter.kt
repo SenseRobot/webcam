a06c49e1072d41455b02c1acb90fe1e50aee44d4
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-06-07 19:26:46 +0000

    updated ignore file

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistsAdapter.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistsAdapter.kt
@@ -4,4 +4,6 @@ import android.arch.paging.PagedListAdapter
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
-import com.evas
+import com.evastos.music.R
+import com.evastos.music.data.model.spotify.item.artist.Artist
+import com.evastos.music.inject.module.GlideRequ
