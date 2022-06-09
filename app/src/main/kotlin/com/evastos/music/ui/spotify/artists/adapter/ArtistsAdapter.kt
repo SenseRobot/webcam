7828dda09e488caaff1f65736cf589be215de334
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-06-09 05:56:17 +0000

    cpufreq: Sync on thread migration optimizations

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistsAdapter.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/ArtistsAdapter.kt
@@ -6,4 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.evastos.music.R
 import com.evastos.music.data.model.spotify.item.artist.Artist
-import com.evastos.music.inject.module.GlideRequ
+import com.evastos.music.inject.module.GlideRequests
+import com.evastos.music.ui.util.extensions.debounceClicks
+import com.evastos.music.ui.util.extensions.inflate
+import com.evasto
