2fa07aaab23c163415ba20a7769a2a4ed5d95f5d
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-06-23 21:13:56 +0000

    Updated the change log.

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/suggestions/ArtistSuggestionsAdapter.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/suggestions/ArtistSuggestionsAdapter.kt
@@ -3,4 +3,7 @@ package com.evastos.music.ui.spotify.artists.adapter.suggestions
 import android.app.SearchManager
 import android.content.Context
 import android.database.MatrixCursor
-import android.
+import android.provider.BaseColumns
+import android.support.v4.widget.SimpleCursorAdapter
+import com.evastos.music.R
+import com.e
