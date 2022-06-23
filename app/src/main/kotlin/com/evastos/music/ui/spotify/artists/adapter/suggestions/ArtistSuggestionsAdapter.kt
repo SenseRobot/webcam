f888dcf8d0f34ffeff69232b48ddf3e2975c7f6f
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-06-23 19:39:09 +0000

    2.6.7

--- app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/suggestions/ArtistSuggestionsAdapter.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/artists/adapter/suggestions/ArtistSuggestionsAdapter.kt
@@ -1,1 +1,6 @@
-package com.evastos.music.ui.spo
+package com.evastos.music.ui.spotify.artists.adapter.suggestions
+
+import android.app.SearchManager
+import android.content.Context
+import android.database.MatrixCursor
+import android.
