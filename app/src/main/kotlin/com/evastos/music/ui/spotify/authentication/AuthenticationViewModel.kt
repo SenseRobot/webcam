0bdda0023d5bff65635c3b9275777243fe0fde47
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-07-03 10:25:16 +0000

    Version 1.1r8

--- app/src/main/kotlin/com/evastos/music/ui/spotify/authentication/AuthenticationViewModel.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/authentication/AuthenticationViewModel.kt
@@ -1,3 +1,5 @@
 package com.evastos.music.ui.spotify.authentication
 
-import com
+import com.evastos.music.domain.Repositories
+import com.evastos.music.ui.base.BaseViewModel
+import com.spotify.sdk.android.authenticat
