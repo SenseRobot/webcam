ccf67e9f0ed21724a9ef594107c61d0892651791
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-07-03 16:43:20 +0000

    Renamed vars

--- app/src/main/kotlin/com/evastos/music/ui/spotify/authentication/AuthenticationViewModel.kt
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/authentication/AuthenticationViewModel.kt
@@ -2,4 +2,9 @@ package com.evastos.music.ui.spotify.authentication
 
 import com.evastos.music.domain.Repositories
 import com.evastos.music.ui.base.BaseViewModel
-import com.spotify.sdk.android.authenticat
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class AuthenticationViewModel
+@Inject co
