73c2026a2e765402788ac753a3b464235c592648
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-07-03 05:40:47 +0000

    Update user manual

---
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/authentication/AuthenticationActivity.kt
@@ -0,0 +1,71 @@
+
+package com.evastos.music.ui.spotify.authentication
+
+import android.annotation.SuppressLint
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.os.Bundle
+import com.evastos.music.R
+import com.evastos.music.ui.base.BaseActivity
+import com.evastos.music.ui.spotify.artists.ArtistsActivity
+import com.evastos.music.ui.util.extensions.setGone
+import com.evastos.music.ui.util.extensions.setVisible
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import kotlinx.android.synthetic.main.activity_authentication.authenticationRootView
+import kotlinx.android.synthetic.main.activity_authentication.networkConnectivityBanner
+
+class AuthenticationActivity : BaseActivity() {
+
+    companion object {
+        private const val AUTH_REQUEST_CODE = 0x11
+    }
+
+    override val layoutRes: Int = R.layout.activity_authentication
+
+    private lateinit var viewModel: AuthenticationViewModel
+
+    @SuppressLint("CheckResult")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.let {
+            title = getString(R.string.activity_title_authentication)
+        }
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(AuthenticationViewModel::class.java)
+
+        viewModel.authRequestLiveEvent.observe(this, Observer { request ->
+            AuthenticationClient.openLoginActivity(this, AUTH_REQUEST_CODE, request)
+        })
+
+        viewModel.authErrorLiveData.observe(this, Observer { error ->
+            error?.let {
+                showSnackbar(authenticationRootView, it, getString(R.string.action_retry)) {
+                    viewModel.onRetry()
+                }
+            }
+            if (error == null) hideSnackbar()
+        })
+
+        viewModel.userLiveEvent.observe(this, Observer { _ ->
+            startActivity(ArtistsActivity.newIntent(this))
+            finish()
+        })
+
+        viewModel.networkConnectivityBannerLiveData.observe(this, Observer { isVisible ->
+            if (isVisible == true) {
+                networkConnectivityBanner.setVisible()
+            } else networkConnectivityBanner.setGone()
+        })
+
+        viewModel.onCreate(networkConnectivityReceiver.observable)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AUTH_REQUEST_CODE) {
+            viewModel.onAuthResponse(AuthenticationClient.getResponse(resultCode, data))
+        }
+    }
+}
---
+++ app/src/main/kotlin/com/evastos/music/ui/spotify/authentication/AuthenticationViewModel.kt
@@ -0,0 +1,3 @@
+package com.evastos.music.ui.spotify.authentication
+
+import com
