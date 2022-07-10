8493c6494f438277e84a2b17f78bccc9e6ec1c80
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-07-10 19:25:56 +0000

    packet: handle too big packets for PACKET_V3

--- app/src/main/kotlin/com/evastos/music/ui/util/extensions/ImageExtensions.kt
+++ app/src/main/kotlin/com/evastos/music/ui/util/extensions/ImageExtensions.kt
@@ -6,4 +6,8 @@ import android.widget.ImageView
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
-import com.bumptech
+import com.bumptech.glide.request.target.Target
+import com.evastos.music.R
+import com.evastos.music.inject.module.GlideRequests
+
+fun GlideRequests.load
