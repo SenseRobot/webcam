bcbd84ebfe18b2cb5d3696b49ff609c3343836be
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-07-27 06:37:29 +0000

    rpmsg: fix dependency on initialization order

--- app/src/main/kotlin/com/evastos/music/ui/util/extensions/ImageExtensions.kt
+++ app/src/main/kotlin/com/evastos/music/ui/util/extensions/ImageExtensions.kt
@@ -44,4 +44,7 @@ fun GlideRequests.loadImage(
                 })
                 .into(imageView)
     } else {
-        imageView.scaleType = ImageView.ScaleType.CENTER_INS
+        imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+        imageView.setImageResource(R.drawable.ic_broken_image)
+    }
+}
---
+++ app/src/main/kotlin/com/evastos/music/ui/util/extensions/StringExtensions.kt
@@ -0,0 +1,3 @@
+package com.evastos.music.ui.util.extensions
+
+private val
