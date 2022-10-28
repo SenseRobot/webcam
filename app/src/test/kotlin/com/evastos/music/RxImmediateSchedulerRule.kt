f90556110e55e9053d0599dd110b4082a1f6e68b
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-10-28 12:29:23 +0000

    print statement

--- app/src/test/kotlin/com/evastos/music/RxImmediateSchedulerRule.kt
+++ app/src/test/kotlin/com/evastos/music/RxImmediateSchedulerRule.kt
@@ -4,4 +4,7 @@ import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
 import org.junit.rules.TestRule
-import org.jun
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class RxImmediateSchedulerRule 
