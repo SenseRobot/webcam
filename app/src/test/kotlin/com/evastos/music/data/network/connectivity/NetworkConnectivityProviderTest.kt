ea294bfa560849942027ea84f9a32c45aaf02e4b
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-11-29 07:04:05 +0000

    Fix reference

--- app/src/test/kotlin/com/evastos/music/data/network/connectivity/NetworkConnectivityProviderTest.kt
+++ app/src/test/kotlin/com/evastos/music/data/network/connectivity/NetworkConnectivityProviderTest.kt
@@ -2,4 +2,7 @@ package com.evastos.music.data.network.connectivity
 
 import android.content.Context
 import android.net.ConnectivityManager
-import and
+import android.net.NetworkInfo
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Assert.assert
