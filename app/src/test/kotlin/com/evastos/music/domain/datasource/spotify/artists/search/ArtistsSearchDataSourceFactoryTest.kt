977e397c8de9d2139dfa6fd86bea680669bad357
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2022-12-13 20:29:46 +0000

    libceph: initialize mon_client con only once

--- app/src/test/kotlin/com/evastos/music/domain/datasource/spotify/artists/search/ArtistsSearchDataSourceFactoryTest.kt
+++ app/src/test/kotlin/com/evastos/music/domain/datasource/spotify/artists/search/ArtistsSearchDataSourceFactoryTest.kt
@@ -1,4 +1,7 @@
 package com.evastos.music.domain.datasource.spotify.artists.search
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
-import android.arch.lifecycle.Observe
+import android.arch.lifecycle.Observer
+import com.evastos.music.data.network.connectivity.NetworkConnectivityProvider
+import com.nhaarman.mockito_kotlin.mock
+import 
