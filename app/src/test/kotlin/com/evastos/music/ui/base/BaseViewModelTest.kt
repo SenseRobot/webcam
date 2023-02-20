49ecba6b06ed2df96b047e70f6517cc5ac01bc12
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2023-02-20 16:15:24 +0000

    Fix project name

--- app/src/test/kotlin/com/evastos/music/ui/base/BaseViewModelTest.kt
+++ app/src/test/kotlin/com/evastos/music/ui/base/BaseViewModelTest.kt
@@ -2,4 +2,7 @@ package com.evastos.music.ui.base
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
 import com.evastos.music.RxImmediateSchedulerRule
-im
+import com.jakewharton.rxrelay2.PublishRelay
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito
