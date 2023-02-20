d17e434cea18cb7c6243cd68f0657faa8599ee6c
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2023-02-20 20:03:18 +0000

    Updating code

--- app/src/test/kotlin/com/evastos/music/ui/base/BaseViewModelTest.kt
+++ app/src/test/kotlin/com/evastos/music/ui/base/BaseViewModelTest.kt
@@ -5,4 +5,8 @@ import com.evastos.music.RxImmediateSchedulerRule
 import com.jakewharton.rxrelay2.PublishRelay
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.times
-import com.nhaarman.mockito
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
+import org.junit.Before
+import org.junit.Rule
+imp
