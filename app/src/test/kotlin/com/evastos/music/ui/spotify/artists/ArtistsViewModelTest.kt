01d077daee4c51979469bf96bc003e45685fbe09
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2023-02-24 08:52:15 +0000

    Revise documentation

--- app/src/test/kotlin/com/evastos/music/ui/spotify/artists/ArtistsViewModelTest.kt
+++ app/src/test/kotlin/com/evastos/music/ui/spotify/artists/ArtistsViewModelTest.kt
@@ -1,3 +1,6 @@
 package com.evastos.music.ui.spotify.artists
 
-import android.arch.core.execut
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android
