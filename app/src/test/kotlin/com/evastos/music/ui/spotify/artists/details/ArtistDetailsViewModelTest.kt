13c1b8f572038e0f0c67a81f79f6dd4da5c57f5d
SenseRobot <111303159+SenseRobot@users.noreply.github.com>
2023-03-21 12:33:37 +0000

    :muscle: bump distribution for dist/snuggsi.min.js

--- app/src/test/kotlin/com/evastos/music/ui/spotify/artists/details/ArtistDetailsViewModelTest.kt
+++ app/src/test/kotlin/com/evastos/music/ui/spotify/artists/details/ArtistDetailsViewModelTest.kt
@@ -3,4 +3,8 @@ package com.evastos.music.ui.spotify.artists.details
 import android.arch.core.executor.testing.InstantTaskExecutorRule
 import android.arch.lifecycle.Observer
 import com.evastos.music.RxImmediateSchedulerRule
-import com.evastos.music.Te
+import com.evastos.music.TestUtil
+import com.jakewharton.rxrelay2.PublishRelay
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import org.ju
